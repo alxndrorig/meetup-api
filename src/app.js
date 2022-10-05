f302e01144424e8bebe6039aae22857722c9b28a
Aliaksandr Yamrom <aliaksandryamrom@gmail.com>
2022-10-05 23:27:12 +0300

    db connection

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import express from "express";
+import "dotenv/config"
 
 class App {
     constructor() {
         this.app = express();
+        this.port = process.env.PORT || 3000
     }
 
     listen() {
-        this.app.listen(3000, () => {
-            console.log('Listen port 3000');
+        this.app.listen(this.port, async () => {
+            console.log(`listen on ${this.port}`);
         });
     }
 }
